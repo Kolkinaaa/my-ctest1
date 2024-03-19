601b751d0a30e6907a310e4e148b1c7df8245668
Kolkinaaa <sofia.kolkina@gmail.com>
2024-03-20 00:50:53 +0300

    coverall

---
+++ tests/input/integration_test.h
@@ -0,0 +1,18 @@
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+
+#include <gtest/gtest.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <match.h>
+extern "C" {
+#include "myfunc.h"
+}
+
+TEST(integrationTest, sqrtFibonachi) {
+    ASSERT_EQ(my_sqrt(fibonachi(4)), sqrt(3));
+    }
+
+#endif 
